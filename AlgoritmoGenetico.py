1a4821ecd05627cab10685fe8d0aaa5c84ed7a8a
Elen Serra <elenserra080@gmail.com>
2022-12-12 00:13:37 -0300

    fitness

--- AlgoritmoGenetico.py
+++ AlgoritmoGenetico.py
@@ -4,37 +4,98 @@ import numpy as np
 
 T = 60                  #periodo
 tamPopulacao = 2        #quatidade de individuos da população 
-tamIndividuo = 3        #quatidade de rotas
-tamCromossomo = 4       #tipos de veiculos
+rotas = 3               #quatidade de rotas
+tipoVeiculos = 3       #tipos de veiculos
 probCruzamento = 0.95   #probabilidade de cruzamento
 probMutacao = 0.1       #probabilidade de mutação
 numGeracoes = 1         #número de gerações
-
+qntdVeiculos = [1,2,1] #veiculos disponiveis para cada tipo 
 
 
 #iniciando uma população aleatoria
-individuo = np.zeros((tamIndividuo, tamCromossomo))  #uma solução
-populacao = []  #conjunto de soluções
+populacao = []  #conjunto de individuos
 
-
-for k in range(tamPopulacao): #tamanho da população
-    #cria um individuo, sorteando valores aleatorios
-    individuo = np.random.randint(T, size=(tamIndividuo, tamCromossomo)) 
-    populacao.append(individuo) #adiciona esse individuo na populacao
+for k in range(tamPopulacao): 
+    #cria um individuo, sorteando valores aleatorios da quantidade de veiculos disponiveis para cada tipo
+    individuo = np.zeros((tipoVeiculos, rotas))
+    for b in range(tipoVeiculos):
+        for r in range(rotas):
+            individuo[b][r] = random.randint(0, qntdVeiculos[b])
+    populacao.append(individuo) 
 
 
 #imprimindo a população
 print("\n __Populacao aleatoria__")
-for i in populacao:
-    print(i, end="\n")
+for individuo in populacao:
+    print(individuo, end="\n")
 print("_"*20)
 
 
+veiculoEnviado = []
+
+K = [100, 45, 67]
+S = 10
+
+for i in range(tamPopulacao):
+    individuo = populacao[i] 
+    for b in range(tipoVeiculos):
+        soma=0
+        for r in range(rotas):  
+            soma = soma +  individuo[b][r]
+        print("Foram enviados ", soma, "veiculos do tipo", b)
+
+        H_br = np.zeros((tipoVeiculos, rotas))
+
+        # checar se veiculosEnviados nao ultrapassa a frota
+        if soma > qntdVeiculos[b]:
+            print("Inviavel") 
+            break
+        else:
+            print("Viavel")
+            for b in range(tipoVeiculos):
+                for r in range(rotas):
+                    if individuo[b][r]==0:
+                        H_br[b][r]=0
+                    else:
+                        H_br[b][r] = T/individuo[b][r]
+                        
+    veiculoEnviado.append(H_br)
+    print("\n")
+    
+for i in veiculoEnviado:
+    print(i, end="\n")
+
+    #custo operacional
+    co = 0
+    for b in range(tipoVeiculos):
+        for r in range(rotas):
+            co = co + K[b] * (individuo[b][r])
+
+    #custo social
+    cs = 0
+    for b in range(tipoVeiculos):
+        for r in range(rotas):
+            cs = cs + S * (H_br[b][r])
+
+#fitness
+fitness = co + cs
+print("Fitness: ", fitness)
+
+
+
+'''
 #variaveis do AG
 aptidao = []
-novaGeracao = np.zeros((tamIndividuo, tamCromossomo))
+novaGeracao = np.zeros((rotas, tipoVeiculos))
 novaPopulacao = []
-individuo=[]
+
+#quantidade total de veículos do tipo b ∈ B disponíveis
+#for k in range(tamPopulacao):
+for i in range(rotas):
+    somaVeiculos=0
+    for j in range(tipoVeiculos):
+        somaVeiculos=somaVeiculos + individuo[i][j]
+        #print(individuo[i][j], end=" ")
 
 #iniciando AG
 geracoes = 1
@@ -43,38 +104,37 @@ while (geracoes<=numGeracoes):
     while (cont<(tamPopulacao-1)): 
 
         #calculo da aptidao dos individuos 
-        totalAptidao = 0
         for i in populacao:
-            novaPopulacao.append(((i*100) + 1000))
-        aptidao = sum(novaPopulacao) #soma das aptidoes 
-        #print(aptidao)
-        
-        for i in aptidao:
-            totalAptidao = totalAptidao+i
-        totalAptidao = sum(totalAptidao) #total da aptidao da populacao
-        
+            novaPopulacao.append(((i*100) + 1000))#calcula para cada individuo a aptdidao
+        totalAptidao = sum(sum(sum(novaPopulacao))) #soma total da aptidao da populacao
+        #print(totalAptidao)
+
+        totalAptidaoInd = 0
+        for i in range(tamPopulacao):
+            totalAptidaoInd = sum(sum(novaPopulacao[i]))#total de aptidao por individuo
+            #print(("totalAptidaoInd", totalAptidaoInd))
 
         #imprimi a população apos o fitness
+        
         print("\n __Populacao com fitness__")
         for i in novaPopulacao:
             print(i, end="\n")
         print("_"*20, "\n")
         
         
-        #identificando a probabilidade de cada individuo
-        probIndividuo = np.zeros((tamIndividuo)) #probabilidade do individuo
-        probIndividuoTotal = np.zeros((tamIndividuo))  #probabilidade do individuo total
-        probIndividuo = sum((1/totalAptidao)*aptidao)
-
+        #identificando a probabilidade de cada individuo ser selecionado
+        probIndividuoTotal = np.zeros((tamPopulacao))  #probabilidade do individuo total
+        probIndividuo = (1/totalAptidao)*totalAptidaoInd
+        #print("probIndividuo", probIndividuo)
 
         #roleta
-        for i in range(tamIndividuo):
+        for i in range(tamPopulacao):
             if (i==0):
-                probIndividuoTotal[i] = probIndividuo[i]
+                probIndividuoTotal[i] = probIndividuo
             else:
-                probIndividuoTotal[i] = probIndividuo[i] + probIndividuoTotal[i-1]
+                probIndividuoTotal[i] = probIndividuo + probIndividuoTotal[i-1]
 
-        print("probTotal: ", probIndividuoTotal)
+        #print("probTotal: ", probIndividuoTotal)
 
         #sorteando os pais de acordo com a probabilidade
         roleta1 = random.uniform(0, 1)
@@ -96,21 +156,22 @@ while (geracoes<=numGeracoes):
                 i=i+1
             pai2=i
 
-        print("pa1 ", pai1)
-        print("pa2 ", pai2)
+        #print("pa1 ", pai1)
+        #print("pa2 ", pai2)
 
         #operação de Crossover
+        
         if (probCruzamento>random.uniform(0, 1)):
-            pontoCorte = round(1+(tamCromossomo-2)*random.uniform(0, 1))
-            print("pc ", pontoCorte)
+            pontoCorte = round(1+(tipoVeiculos-2)*random.uniform(0, 1))
+            #print("pc ", pontoCorte)
             gene11 = populacao[pai1][0:pontoCorte]
-            print("g1 ",gene11)
-            gene12 = populacao[pai1][pontoCorte:tamCromossomo]
-            print("g2 ",gene12)
+            #print("g1 ",gene11)
+            gene12 = populacao[pai1][pontoCorte:tipoVeiculos]
+            #print("g2 ",gene12)
             gene21 = populacao[pai2][0:pontoCorte]
-            print("g3 ", gene21)
-            gene22 = populacao[pai2][pontoCorte:tamCromossomo]
-            print("g4 ", gene22)
+            #print("g3 ", gene21)
+            gene22 = populacao[pai2][pontoCorte:tipoVeiculos]
+            #print("g4 ", gene22)
             filho1 = np.concatenate((gene11,gene22), axis=None)
             filho2 = np.concatenate((gene21,gene12), axis=None)
 
@@ -123,7 +184,7 @@ while (geracoes<=numGeracoes):
 
         #mutação
         if (probMutacao>random.uniform(0, 1)):            
-            d = round(1+ (tamCromossomo-2)*random.uniform(0, 1))
+            d = round(1+ (tipoVeiculos-2)*random.uniform(0, 1))
             if (novaGeracao[novosIndividuos-2][d] == 0):
                 novaGeracao[novosIndividuos-2][d] = 1
             else:
@@ -140,9 +201,10 @@ while (geracoes<=numGeracoes):
 
 
 
-"""
+
 #nova geracao apos o AG
 print("\n ___ Nova Geracao___")
 for i in populacao:
     print(i, end="\n")
-"""
+
+'''
